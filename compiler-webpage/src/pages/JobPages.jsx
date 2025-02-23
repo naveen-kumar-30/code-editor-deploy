df095a418c113c9f474f2bab753bb259e62a59ab
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-02-23 10:25:42 +0530

    Delete compiler-webpage directory

--- compiler-webpage/src/pages/JobPages.jsx
+++
@@ -1,9 +0,0 @@
-import JobListing from "../components/JobListing"
-const JobPages = () => {
-  return (
-    <section className="bg-blue-50 px-4 py-6"> 
-      <JobListing/>
-    </section>
-  )
-}
-export default JobPages
